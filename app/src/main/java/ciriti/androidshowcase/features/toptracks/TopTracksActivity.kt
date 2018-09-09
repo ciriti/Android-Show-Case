9b14021938c1ae3d7653e3adbc41ad26aef5fee1
ciriti <ciriti@gmail.com>
2018-09-09 14:05:42 +0200

    fixed latyout bug

--- app/src/main/java/ciriti/androidshowcase/features/toptracks/TopTracksActivity.kt
+++ app/src/main/java/ciriti/androidshowcase/features/toptracks/TopTracksActivity.kt
@@ -1,7 +1,9 @@
 package ciriti.androidshowcase.features.toptracks
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import ciriti.androidshowcase.R
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
@@ -14,5 +16,10 @@ class TopTracksActivity : AppCompatActivity(), HasSupportFragmentInjector {
 
   @Inject lateinit var fragmentInjector: DispatchingAndroidInjector<Fragment>
   override fun supportFragmentInjector(): AndroidInjector<Fragment> = fragmentInjector
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
 
 }
